2bc26565983f169cb114b97fbcc4694bb65289ec
russellborja <russellborja@gmail.com>
2015-04-19 21:25:57 -0400

    added pictures in gallery

--- js/bootstrap-image-gallery.js
+++ js/bootstrap-image-gallery.js
@@ -62,6 +62,7 @@
                     modal.addClass('in');
                 }, factoryInterface);
             modal.find('.modal-title').text(element.title || String.fromCharCode(160));
+            console.log(element.title)
             modal.find('.modal-body').append(element);
             return modal[0];
         },
